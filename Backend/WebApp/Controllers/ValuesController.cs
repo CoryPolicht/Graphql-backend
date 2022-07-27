d1444e963afa29dafda8c8b74c1cbe9913e29536
Cory <cory.policht@gmail.com>
2022-07-27 12:04:23 -0500

    init commit

---
+++ Backend/WebApp/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Articles : ControllerBase
+    {
+        public IEnumerable<ArticleView> GetArticles()
+        {
+
+        }
+
+        public bool DeleteArticle (int id)
+        {
+
+        }
+    }
+}
